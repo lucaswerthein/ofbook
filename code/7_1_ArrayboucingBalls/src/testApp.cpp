ee7af8fe391b7e57958c96261e3c4bd4561133aa
lucaswerthein <wsacul@hotmail.com>
2010-06-08 10:25:26 -0400

    updateting 7_1

--- code/7_1_ArrayboucingBalls/src/testApp.cpp
+++ code/7_1_ArrayboucingBalls/src/testApp.cpp
@@ -1,58 +1,76 @@
 #include "testApp.h"
-
 //--------------------------------------------------------------
 void testApp::setup(){
-	ofBackground(0, 0, 0);
-	ofSetColor(255, 255, 255);
-
+    ofBackground(0, 0, 0); 
+    ofSetFrameRate(60); 
+    ofSetCircleResolution(64); 
+    ofEnableAlphaBlending();    
+   
+    for(int i = 0; i < NUM; i++){
+        xPos[i] = ofRandom(0, ofGetWidth()); 
+		yPos[i] = ofRandom(0, ofGetHeight()); 
+        xSpeed[i] = ofRandom(-6, 6); 
+        ySpeed[i] = ofRandom(-6, 6);     }
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
-
+    
+    for(int i = 0; i < NUM; i++){
+        xPos[i] = xPos[i] + xSpeed[i]; 
+        yPos[i] = yPos[i] + ySpeed[i];
+        
+        
+        if(xPos[i] < 0 || xPos[i] > ofGetWidth()){
+            xSpeed[i] = xSpeed[i] * -1;
+        }
+        if(yPos[i] < 0 || yPos[i] > ofGetHeight()){
+            ySpeed[i] = ySpeed[i] * -1;
+        }
+    }
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
-	if(mouseX > ofGetWidth()/2){
-		ofRect(ofGetWidth()/2, 0, ofGetWidth(), ofGetHeight());
-	}else{
-	 ofRect(0, 0, ofGetWidth()/2,ofGetHeight());	
-	}
+    ofSetColor(200, 40, 150, 100);     
+    for(int i = 0; i < NUM; i++){
+        ofCircle(xPos[i], yPos[i], 20); 
+    }
+	
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::windowResized(int w, int h){
-
+    
 }
 
